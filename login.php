17ee1e08a30a8c4f3bb929e9732b44c05e800d79
windaangellina <wau959@gmail.com>
2020-04-07 20:01:51 +0700

    cek

--- login.php
+++ login.php
@@ -12,12 +12,7 @@
 
         <!-- CSS template login -->
         <!--===============================================================================================-->
-        <link rel="icon" type="image/png" href="images/login/icons/favicon.ico" />
-        <!--===============================================================================================-->
-        <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/all.css">
-        <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/all.min.css">
-        <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.min.css">
+        <link rel="icon" type="image/png" href="images/login/icons/favicon.ico" />        
         <!--===============================================================================================-->
         <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
         <!--===============================================================================================-->
@@ -35,35 +30,14 @@
         <link rel="stylesheet" type="text/css" href="style/login/main.css">
         <!--===============================================================================================-->
 
-        <!-- JS Library Import -->
-        <script src="js/jquery-3.4.1.min.js"></script>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/jQueryUI.js"></script>
-        <script type="text/javascript" src="js/datatables.js"></script>
-
-        <!-- JS template login -->
-        <!--===============================================================================================-->
-        <script src="vendor/login/animsition/js/animsition.min.js"></script>
-        <!--===============================================================================================-->
-        <script src="vendor/login/select2/select2.min.js"></script>
-        <!--===============================================================================================-->
-        <script src="vendor/login/daterangepicker/moment.min.js"></script>
-        <script src="vendor/login/daterangepicker/daterangepicker.js"></script>
-        <!--===============================================================================================-->
-        <script src="vendor/login/countdowntime/countdowntime.js"></script>
-        <!--===============================================================================================-->
-        <script src="js/login.js"></script>
-
         <!-- CSS Sendiri -->
         <link href="style/lrc.css" rel="stylesheet">
-
-        <!-- JS Sendiri -->
 
         <title>Sign In Page</title>
     </head>
     <body>
         <div class="limiter">
-            <div class="container-login100" style="background-image: url('images/login//bg-06.jpg');">
+            <div class="container-login100" style="background-image: url('images/login/bg-06.jpg');">
                 <div class="wrap-login100 p-t-30 p-b-50">
                     <span class="login100-form-title p-b-41">
                         Account Login
@@ -93,9 +67,26 @@
             </div>
         </div>
 
+
         <div id="dropDownSelect1"></div>
 
-        
+        <!-- JS Library Import -->
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/jQueryUI.js"></script>
+        <script type="text/javascript" src="js/datatables.js"></script>
 
+        <!-- JS template login -->
+        <!--===============================================================================================-->
+        <script src="vendor/login/animsition/js/animsition.min.js"></script>
+        <!--===============================================================================================-->
+        <script src="vendor/login/select2/select2.min.js"></script>
+        <!--===============================================================================================-->
+        <script src="vendor/login/daterangepicker/moment.min.js"></script>
+        <script src="vendor/login/daterangepicker/daterangepicker.js"></script>
+        <!--===============================================================================================-->
+        <script src="vendor/login/countdowntime/countdowntime.js"></script>
+        <!--===============================================================================================-->
+        <script src="js/login.js"></script>
     </body>
 </html>
