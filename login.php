1d4d405acfabf479c3f5c9a311a7d2152b232ffb
windaangellina <wau959@gmail.com>
2020-04-07 19:34:30 +0700

    nambahin resources font awesome dan folder images

--- login.php
+++ login.php
@@ -10,11 +10,49 @@
         <link rel="stylesheet" type="text/css" href="css/datatables.css"/>
         <link href="css/all.css" rel="stylesheet">
 
+        <!-- CSS template login -->
+        <!--===============================================================================================-->
+        <link rel="icon" type="image/png" href="images/login/icons/favicon.ico" />
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/all.css">
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/all.min.css">
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/animate/animate.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/css-hamburgers/hamburgers.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/animsition/css/animsition.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/select2/select2.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/daterangepicker/daterangepicker.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="style/login/util.css">
+        <link rel="stylesheet" type="text/css" href="style/login/main.css">
+        <!--===============================================================================================-->
+
         <!-- JS Library Import -->
         <script src="js/jquery-3.4.1.min.js"></script>
         <script src="js/bootstrap.bundle.min.js"></script>
         <script src="js/jQueryUI.js"></script>
         <script type="text/javascript" src="js/datatables.js"></script>
+
+        <!-- JS template login -->
+        <!--===============================================================================================-->
+        <script src="vendor/login/animsition/js/animsition.min.js"></script>
+        <!--===============================================================================================-->
+        <script src="vendor/login/select2/select2.min.js"></script>
+        <!--===============================================================================================-->
+        <script src="vendor/login/daterangepicker/moment.min.js"></script>
+        <script src="vendor/login/daterangepicker/daterangepicker.js"></script>
+        <!--===============================================================================================-->
+        <script src="vendor/login/countdowntime/countdowntime.js"></script>
+        <!--===============================================================================================-->
+        <script src="js/login.js"></script>
 
         <!-- CSS Sendiri -->
         <link href="style/lrc.css" rel="stylesheet">
@@ -24,17 +62,40 @@
         <title>Sign In Page</title>
     </head>
     <body>
-    <div class="container">
-        <div class="row">
-        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div class="card card-signin my-5">
-            <div class="card-body">
-                <h5 class="card-title text-center">Sign In</h5>
-                
-            </div>
+        <div class="limiter">
+            <div class="container-login100" style="background-image: url('images/login//bg-06.jpg');">
+                <div class="wrap-login100 p-t-30 p-b-50">
+                    <span class="login100-form-title p-b-41">
+                        Account Login
+                    </span>
+                    <form class="login100-form validate-form p-b-33 p-t-5">
+                        <div class="wrap-input100 validate-input" data-validate="Enter username">
+                            <input class="input100" type="text" name="username" placeholder="User name">
+                            <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                        </div>
+
+                        <div class="wrap-input100 validate-input" data-validate="Enter password">
+                            <input class="input100" type="password" name="pass" placeholder="Password">
+                            <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+                        </div>
+
+                        <div class="container-login100-form-btn m-t-32 d-flex justify-content-around">
+                            <button class="login100-form-btn">
+                                Login
+                            </button>
+                            <button class="login100-form-btn">
+                                Register
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </div>
-        </div>
-    </div>
+
+        <div id="dropDownSelect1"></div>
+
+        
+
     </body>
 </html>
