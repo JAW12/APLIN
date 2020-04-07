913c0acefa07d16f6c43786bb53be1ebb6c4f0d4
JAW12 <jem.angkasa91@gmail.com>
2020-04-07 17:36:03 +0700

    Fixes #1 -> Folder Structure e tak benerno lgi, jdine untuk css khusus library tak masukno nde css, sedangkan css khusus kita dewe nde folder style, begitupun JS File.
    Closes #2 ->
    Library yang tak masukkin:
    - Bootstrap
    - JQuery
    - Popper
    - JQuery UI
    - Datatables
    - Font Awesome
    
    Closes #6 ->
    a. Untuk Style General bisa dilihat di style/Index.css
    b. Untuk Style Login, Register, Confirmation Page cek link ini:
    https://startbootstrap.com/snippets/login/
    .css nya di style/lrc.css, kalau ada kalian mw tambahin sndiri buat sndiri file nya ya cnth. style/login.css.

---
+++ verification.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- CSS Library Import -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="asset/css/jquery-ui.css">
+        <link rel="stylesheet" type="text/css" href="css/datatables.css"/>
+        <link href="css/all.css" rel="stylesheet">
+
+        <!-- JS Library Import -->
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/jQueryUI.js"></script>
+        <script type="text/javascript" src="js/datatables.js"></script>
+
+        <!-- CSS Sendiri -->
+        <link href="style/lrc.css" rel="stylesheet">
+
+        <!-- JS Sendiri -->
+
+        <title>Register Page</title>
+    </head>
+    <body>
+    <div class="container">
+        <div class="row">
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div class="card card-signin my-5">
+            <div class="card-body">
+                <h5 class="card-title text-center">Register</h5>
+                
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </body>
+</html>
