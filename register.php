c61d951280a982d09f5ddea7938016733db030b6
piekoe <hongary.kevin@gmail.com>
2020-04-09 19:08:45 +0800

    progres #8

--- register.php
+++ register.php
@@ -11,11 +11,26 @@
         <link href="css/all.css" rel="stylesheet">
         <link rel="icon" type="image/png" href="res/img/goblin.png" />     
 
-        <!-- JS Library Import -->
-        <script src="js/jquery-3.4.1.min.js"></script>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/jQueryUI.js"></script>
-        <script type="text/javascript" src="js/datatables.js"></script>
+     
+        <!-- CSS template login -->
+        <!--===============================================================================================-->        
+        <link rel="icon" type="image/png" href="res/img/goblin.png" />     
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/animate/animate.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/css-hamburgers/hamburgers.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/animsition/css/animsition.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/select2/select2.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/login/daterangepicker/daterangepicker.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="style/login/util.css">
+        <link rel="stylesheet" type="text/css" href="style/login/main.css">
+        <!--===============================================================================================-->
 
         <!-- CSS Sendiri -->
         <link href="style/lrc.css" rel="stylesheet">
@@ -25,17 +40,78 @@
         <title>Register Page</title>
     </head>
     <body>
-    <div class="container">
-        <div class="row">
-        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div class="card card-signin my-5">
-            <div class="card-body">
-                <h5 class="card-title text-center">Register</h5>
-                
-            </div>
+    <div class="limiter">
+            <div class="container-login100" style="background-image: url('res/img/login/bg-06.jpg');">
+                <div class="wrap-login100 p-t-30 p-b-50">      
+                    <!-- <span class="login100-form-title p-b-41">
+                        Account Login
+                    </span> -->
+                    <div class="card rounded">
+                        <div class="text-center mt-5">
+                            <img src="res/img/logo.png" class="card-img mx-auto w-50 h-50" alt="...">
+                        </div>
+                        <div class="card-body" style="box-sizing: border-box">                        
+                            <form class="login100-form validate-form p-b-33 p-t-5">
+                                <div class="wrap-input100 validate-input" data-validate="Enter Username">
+                                    <input class="input100" type="text" name="username" placeholder="Username">
+                                    <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                                </div>
+
+                                <div class="wrap-input100 validate-input" data-validate="Enter Password">
+                                    <input class="input100" type="password" name="pass" placeholder="Password">
+                                    <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+                                </div>
+                                <div class="wrap-input100 validate-input" data-validate="Enter Email">
+                                    <input class="input100" type="text" name="email" placeholder="Email">
+                                    <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                                </div>
+                                <div class="wrap-input100 validate-input" data-validate="Enter First Name">
+                                    <input class="input100" type="text" name="firstname" placeholder="First Name">
+                                    <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                                </div>
+                                <div class="wrap-input100 validate-input" data-validate="Enter Last Name">
+                                    <input class="input100" type="text" name="lastname" placeholder="Last Name">
+                                    <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                                </div>
+                                
+                                <div class="container-login100-form-btn m-t-32 d-flex justify-content-around">
+                                    <button class="login100-form-btn w-75">
+                                        Register
+                                    </button>
+                                </div>
+
+                                <div class="container text-center">
+                                    <button class="btn btn-link mt-4 text-decoration-none">
+                                        <span class="text-secondary mt-2">Don't have an account?</span>
+                                        <span class="text-primary font-weight-bold"> &nbsp;Sign Up </span>
+                                    </button>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                    
+                    
+                </div>
             </div>
         </div>
-        </div>
-    </div>
+
+       <!-- JS Library Import -->
+       <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/jQueryUI.js"></script>
+        <script type="text/javascript" src="js/datatables.js"></script>
+        <!-- JS template login -->
+        <!--===============================================================================================-->
+        <script src="vendor/login/animsition/js/animsition.min.js"></script>
+        <!--===============================================================================================-->
+        <script src="vendor/login/select2/select2.min.js"></script>
+        <!--===============================================================================================-->
+        <script src="vendor/login/daterangepicker/moment.min.js"></script>
+        <script src="vendor/login/daterangepicker/daterangepicker.js"></script>
+        <!--===============================================================================================-->
+        <script src="vendor/login/countdowntime/countdowntime.js"></script>
+        <!--===============================================================================================-->
+        <script src="js/login.js"></script>
     </body>
 </html>
