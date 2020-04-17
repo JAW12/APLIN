b9865bf99d617ea0cbcc567c994477417747366c
hendralw12 <hendralw3@gmail.com>
2020-04-17 21:39:58 +0700

    product-detail sementara

--- product-detail.php
+++ product-detail.php
@@ -23,17 +23,93 @@
         }
     }
 ?>
-<div>
-    <div style="width: 40%">
-        <img src="<?= $fotoProduk?>" width="300px" height="300px"/>
-    </div>
-    <div style="width: 40%">
-        <h1><?=$namaProduk?></h1>
-        <h2><?=$hargaProduk?></h2>
-    </div>
-    <form method="POST">
-        <input type="hidden" name="idProduk" value="<?=$idProduk?>">
-        <input type="text" name="jumlahBeliProduk" placeholder="1">
-        <button type="submit" name="btnBeli">Beli Sekarang</button>
-    </form>
-</div>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- CSS Library Import -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="asset/css/jquery-ui.css">
+        <link rel="stylesheet" type="text/css" href="css/datatables.css"/>
+        <link href="css/all.css" rel="stylesheet">
+        <link rel="icon" type="image/png" href="res/img/goblin.png" />    
+         
+        <!-- JS Library Import -->
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/jQueryUI.js"></script>
+        <script type="text/javascript" src="js/datatables.js"></script>
+        <title>Detail Produk</title>
+        <script>
+            $( function() {
+                $( "#tabs" ).tabs();
+            } );
+        </script>
+    </head>
+    <body id="page-top">
+        <header>
+            <nav class="navbar navbar-expand-lg bg-light navbar-light fixed-top py-3" id="mainNav">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">Logo</a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0 mr-4">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">About Us</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">Contact Us</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">Products</a></li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0 mr-3">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                    <a class="btn btn-primary" href="login.php" role="button">Login</a>
+                </div>
+            </nav>
+        </header>
+
+        <main>
+            <div style="width: 40%">
+                <img src="<?= $fotoProduk?>" width="300px" height="300px"/>
+            </div>
+            <div style="width: 40%">
+                <h1><?=$namaProduk?></h1>
+                <h2><?=$hargaProduk?></h2>
+            </div>
+            <form method="POST">
+                <input type="hidden" name="idProduk" value="<?=$idProduk?>">
+                <input type="text" name="jumlahBeliProduk" placeholder="1">
+                <button type="submit" class="btn btn-success" name="btnBeli">Beli Sekarang</button>
+            </form>
+            <div id="tabs">
+                <ul>
+                    <li><a href="#tabs-1">Nunc tincidunt</a></li>
+                    <li><a href="#tabs-2">Proin dolor</a></li>
+                    <li><a href="#tabs-3">Aenean lacinia</a></li>
+                </ul>
+                <div id="tabs-1">
+                    <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+                </div>
+                <div id="tabs-2">
+                    <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+                </div>
+                <div id="tabs-3">
+                    <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+                </div>
+            </div>
+            <div class="text-center my-3">
+                <a class="btn btn-lg btn-dark" href="" role="button">CONTACT US</a>
+            </div>
+        </main>
+
+        <footer class="bg-dark py-5">
+            <div class="container">
+                <div class="medium text-center text-light">
+                    Copyright ©2020 - Squee Store
+                </div>
+                <div class="small text-center text-light">
+                    Squee Store berusaha menyediakan berbagai macam peralatan dan perlengkapan bahan bangunan dengan kualitas terjamin dan terjangkau.
+                </div>   
+            </div>
+        </footer>
+    </body>
+</html>
